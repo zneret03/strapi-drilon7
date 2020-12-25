0dfaf730707d319ac1c20e8959992c66fef695fa
Ian <DrilonIan@yahoo.com>
2020-12-25 21:39:06 +0800

    first commit

---
+++ config/database.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        client: 'mongo',
+        uri  : `mongodb://zneret03:${process.env.PASSWORD}@drilon7-shard-00-00.22nxh.mongodb.net:27017,drilon7-shard-00-01.22nxh.mongodb.net:27017,drilon7-shard-00-02.22nxh.mongodb.net:27017/<drilon7>?ssl=true&replicaSet=atlas-fcix3l-shard-0&authSource=admin&retryWrites=true&w=majority`
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '41dedf4c-b6fc-4681-bb82-2dedad24ad32'
+};
