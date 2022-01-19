a8c505ac185bae7e294101acf8f6dfc86938c84c
Ian <DrilonIan@yahoo.com>
2022-01-19 21:08:08 +0800

    changing password

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        client: 'mongo',
-        uri  : `mongodb://zneret03:${process.env.PASSWORD}@drilon7-shard-00-00.22nxh.mongodb.net:27017,drilon7-shard-00-01.22nxh.mongodb.net:27017,drilon7-shard-00-02.22nxh.mongodb.net:27017/<drilon7>?ssl=true&replicaSet=atlas-fcix3l-shard-0&authSource=admin&retryWrites=true&w=majority`
+        client: "mongo",
+        uri: `mongodb://zneret03:9UXu6TB8yR8pSaJP@drilon7-shard-00-00.22nxh.mongodb.net:27017,drilon7-shard-00-01.22nxh.mongodb.net:27017,drilon7-shard-00-02.22nxh.mongodb.net:27017/<drilon7>?ssl=true&replicaSet=atlas-fcix3l-shard-0&authSource=admin&retryWrites=true&w=majority`,
       },
       options: {
         useNullAsDefault: true,
